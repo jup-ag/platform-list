e80c12cdec9a10800a37a1c90414c19c75aabf17
binhnguyen2501 <binhq6@gmail.com>
2026-02-09 18:40:42 +0700

    feat: add lpagent platform

--- src/platforms/lpagent.ts
+++ src/platforms/lpagent.ts
@@ -11,7 +11,7 @@ export const platform: PlatformRaw = {
     github: "https://github.com/lpagent",
     documentation: "https://docs.lpagent.io/",
   },
-  tags: ["dapp", "tool"],
+  tags: ["dapp", "tool", "defi"],
   isDeprecated: true,
 };
 export const services: ServiceRaw[] = [];
