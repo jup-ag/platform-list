ff46dcb6d8012808310b4e80671c9c54f580cf26
opptylabs <oppty@proton.me>
2026-02-12 16:47:03 +0100

    add tramplin

--- src/platforms/tramplin.ts
+++ src/platforms/tramplin.ts
@@ -1,29 +1,14 @@
-import { PlatformRaw, ServiceRaw, NetworkId } from "../types";
+import { PlatformRaw, ServiceRaw } from "../types";
 export const platform: PlatformRaw = {
-  id: "solanaid",
-  name: "Solana ID",
-  description: "Decentralized identity and naming service on Solana",
+  id: "tramplin",
+  name: "Tramplin",
+  description: "Boost your savings with randomized yield.",
   links: {
-    website: "https://app.solana.id/",
-    twitter: "https://x.com/solanaidentity",
-    discord: "https://discord.com/invite/solanaid",
-    telegram: "https://t.me/+NKjmb1u7LZw5YTU6",
+    website: "https://tramplin.io/",
+    twitter: "https://x.com/Tramplin_io",
+    discord: "https://discord.com/invite/tramplin",
   },
   tags: ["dapp"],
-  platformToken: "SoLiDMWBct5TurG1LNcocemBK7QmTn4P33GSrRrcd2n",
 };
 
-const contract = {
-  name: "Staking",
-  address: "gp8fqiE5cwX3JRT8unpKeFutNdMihyisAe3nx6L3S1p",
-  networkId: NetworkId.solana,
-};
-
-export const service: ServiceRaw = {
-  id: "solanaid-staking",
-  name: "Staking",
-  platformId: platform.id,
-  contractsRaw: [contract],
-};
-
-export const services: ServiceRaw[] = [service];
+export const services: ServiceRaw[] = [];
