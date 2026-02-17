378cc4839623e3ba9103f93795d73eafc0c4b612
Felipe Quintero <felipequintergoma@gmail.com>
2026-02-17 09:42:44 -0500

    add smithii platform

---
+++ src/platforms/smithii.ts
@@ -0,0 +1,20 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+
+export const platform: PlatformRaw = {
+  id: "smithii",
+  name: "Smithii",
+  description:
+    "Smithii is an all-in-one suite of tools designed for blockchain token creators to launch, edit, and manage the liquidity of their crypto projects quickly, affordably, and without the need for programming knowledge.",
+  defillamaId: "smithii",
+  links: {
+    website: "https://tools.smithii.io/",
+    twitter: "https://x.com/SmithiiTools",
+    discord: "https://discord.gg/fgwUfZT9Qw",
+    documentation: "https://docs.smithii.io/",
+    github: "https://github.com/SmithiiDev",
+  },
+  tags: ["dapp", "tool", "launchpad"],
+};
+
+export const services: ServiceRaw[] = [];
+export default services;
