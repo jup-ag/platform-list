aa9dca0bd12f5436d0011a4aa3ec63d23ae7314e
opptylabs <oppty@proton.me>
2026-03-24 11:29:54 +0100

    add yo

--- src/platforms/yo.ts
+++ src/platforms/yo.ts
@@ -1,12 +1,28 @@
-import { PlatformRaw, ServiceRaw } from "../types";
+import { NetworkId, PlatformRaw, ServiceRaw } from "../types";
 export const platform: PlatformRaw = {
-  id: "yield",
-  name: "Yieldfan",
-  isDeprecated: true,
+  id: "yo",
+  name: "YO",
+  description:
+    "YO is a DeFi protocol that helps you easily boost your crypto earnings without the hassle.",
   links: {
-    website: "https://yield.fan/dashboard",
-    twitter: "https://x.com/yieldfan",
+    website: "https://app.yo.xyz/",
+    twitter: "https://x.com/yield",
+    documentation: "https://docs.yo.xyz/",
   },
   tags: ["dapp"],
 };
-export const services: ServiceRaw[] = [];
+
+const contract = {
+  name: "SOL Vault",
+  address: "yvSoLSBaLoqZ2yQttGbaYzHDXr9Bo9UdqtiRDiVaMxP",
+  networkId: NetworkId.solana,
+};
+
+export const solVaultService: ServiceRaw = {
+  id: `${platform.id}-sol-vault`,
+  name: "yoSOL Vault",
+  platformId: platform.id,
+  contractsRaw: [contract],
+};
+
+export const services: ServiceRaw[] = [solVaultService];
